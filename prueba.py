b5116958dd8f7134e070431c7924741e55faccd1
sergio <al.sergiomacias@iesramonllull.net>
2023-10-31 13:02:43 +0100

    mi primer repositorio

---
+++ prueba.py
@@ -0,0 +1,18 @@
+print("hola mundo")
+#soy un comentario
+"""texto que no se va a interpretar"""
+texto = "esto es una variable"
+nombre = "soy sergio"
+altura = "160"
+year = "2023"
+print(f"{texto} - {nombre} - {year}")
+#ENtrada
+#sitioweb = input("¿cual es tu pagina web?: ")
+#print("El sitio web del ususario es: " + sitioweb)
+#coniciones
+altura = int(input("¿cuanto mides?: "))
+
+if altura >= 180:
+    print("eres alto")
+else:
+    print("eres bajo")
